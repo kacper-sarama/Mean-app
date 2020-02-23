18bdbb1240a1cf53b50a6bb701a3733e7ec649da
ksarama <ksarama@andea.com>
2020-02-23 15:08:44 +0100

    server (1 ver.) set

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const http = require('http');
+const app = require('./backend/app');
 
-http.createServer();
+const port = process.env.PORT || 3000;
+
+app.set('port', port)
+const server = http.createServer(app);
+
+server.listen(port);
